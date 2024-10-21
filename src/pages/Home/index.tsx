5d6f1717f6b308e36043b82d312cb356e065f106
Sara-MF <sara.melo.dev@gmail.com>
2024-10-21 09:23:41 -0300

    criando sessao de recomendacao de livro

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -31,13 +31,20 @@ export function Home() {
         <>
             <Header />
             <Container>
-                <SectionTitle title='O que você quer ler hoje?' />
-                <div className="gap-4 grid sm:grid-cols-4 md:grid-cols-9">
-                    {booksGender.map(book =>
-                        <Button title={book}
-                                variant={selectedGender.includes(book) ? 'dark' : 'light'} 
-                                onClick={() => handleSelect(book)} />
-                    )}
+                <div>
+                    <SectionTitle title='O que você quer ler hoje?' />
+                    <div className="gap-4 grid sm:grid-cols-4 md:grid-cols-9">
+                        {booksGender.map(book =>
+                            <Button title={book}
+                                    variant={selectedGender.includes(book) ? 'dark' : 'light'} 
+                                    onClick={() => handleSelect(book)} />
+                        )}
+                    </div>
+                </div>
+                <div>
+                    <p className="text-zinc-700 text-2xl mt-8 mb-5">Sobre o que você gostaria de receber uma recomendação de livro hoje?</p>
+                    <input className="w-full outline-1 outline-emerald-500 py-2 px-4 rounded-lg mb-16 border-2 border-slate-300 shadow-lg"
+                        type="text" placeholder="Eu gostaria de ler sobre..." />
                 </div>
             </Container>
         </>
